ace7d70fb043292d61f23e0f51bc8e4ba05dc67b
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-05-24 00:06:25 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import icon from './assets/iconLogo.svg'
+
+function App() {
+  
+
+  return (
+    <>
+    <img src={ icon } alt="Shevchenko logo" style={{ width: '200px' }} />
+      <p>Просвіта м.Винники</p>
+    </>
+  )
+}
+
+export default App;
